46492c9d54f721a5fe92e37f98779aed2825295d
user <user@DESKTOP-T9EEOQO>
2024-10-31 19:24:08 +0300

    Добавьте файлы проекта.

---
+++ client2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+string ip = "127.0.0.1";
+int port = 8888;
+
+SendMessageToHost(ip, port);
+
+async Task<Socket?> ConnectToHostAsync(string url, int port)
+{
+    Socket? socke = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    try
+    {
+        await socke.ConnectAsync(url, port);
+        return socke;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Не подключился\n" + ex.Message);
+        socke.Close();
+    }
+    return null;
+}
+
+async Task<string> SendMessageToHost(string ip, int port)
+{
+    using Socket? sockee = await ConnectToHostAsync(ip, port);
+    if (sockee is null)
+        return $"Не законнектился";
+
+
+    //////
+
+
+    Console.Write("\nВведи текст для отправки\n");
+    var message = Console.ReadLine();
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    await sockee.SendAsync(messageBytes);
+
+    sockee.Shutdown(SocketShutdown.Send);
+
+    var massBytes = new byte[512];
+    var builder = new StringBuilder();
+    int bytes;
+
+    do
+    {
+        bytes = await sockee.ReceiveAsync(massBytes);
+        string respText = Encoding.UTF8.GetString(massBytes, 0, bytes);
+        builder.Append(respText);
+    }
+    while (bytes > 0);
+
+    await sockee.DisconnectAsync(true); // discon with host
+    return builder.ToString();
+}
---
+++ server1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+
+LinkedList<string> values = new LinkedList<string>();
+
+int port_server = 8888;
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port_server);
+using (Socket socke_server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+    try
+    {
+        socke_server.Bind(ipPoint);
+        socke_server.Listen(1000);
+        Console.WriteLine("Сервер запущен, Ожидаем...");
+
+        using Socket client = await socke_server.AcceptAsync();
+        Console.WriteLine($"IP подключения: {client.RemoteEndPoint}");
+
+        var buffer_receive = new byte[512];
+        StringBuilder stringBuilder = new StringBuilder();
+        int bytes;
+
+        do
+        {
+            bytes = await client.ReceiveAsync(buffer_receive);
+            var messageBytes = Encoding.UTF8.GetString(buffer_receive, 0, bytes);
+            stringBuilder.Append(messageBytes);
+        }
+        while (bytes > 0);
+        Console.WriteLine(stringBuilder.ToString());
+    }
+    catch (SocketException e)
+    {
+        Console.WriteLine("Принудительное отключение хоста");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
